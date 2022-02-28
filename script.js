562d4f0297b80a53db2a4d8bdcf6fea0fcb273b7
Daniel <danielz.nas@gmail.com>
2022-02-27 23:26:51 -0400

    added a listner for the numpad keys that logs the key pressed and prints it in the displayText window

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 //    -------         DOM CASH        ------
+const displayText = document.querySelector('#displayText');
 //    -------            UI           ------
+window.addEventListener('keydown', (e) => {
+    displayText.textContent = e.key;
+    console.log(e.key);
+})
 //    -------           LOGIC         ------
+function sum (a, b) {
+    let sum = a + b;
+    return sum;
+}
+
+function subtration(a, b) {
+    let subtration = a - b;
+    return subtration;
+}
+
+function multiplication(a, b) {
+    let multiplication = a * b;
+    return multiplication;
+}
+
+function division(a, b) {
+    let division = a / b; 
+    return division;
+}
