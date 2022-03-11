107cd993d0c97f3c559ed209e4dbfe38dc464590
Daniel <danielz.nas@gmail.com>
2022-03-11 17:30:52 -0400

    added the function operate and the operation functions

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 //    -------         DOM CASH        ------
 const displayText = document.querySelector('#displayText');
 const clearButton = document.querySelector('#clear');
+
 //    -------            UI           ------
 window.addEventListener('keydown', (e) => {
     if(!e.code.includes('Numpad')) {return}
+    // if(e.keyCode < 96 || e.keyCode > 105) {return}
     memoryOne.push(e.key)
     displayText.textContent = memoryOne.join('');
-    console.log(memoryOne);
+    console.log(e.keyCode);
 })
 
 clearButton.addEventListener('click', () => {
     memoryOne = [];
     displayText.textContent = memoryOne.join('');
     console.log(memoryOne);
-
 })
-//    -------           LOGIC         ------
 
+//    -------           LOGIC         ------
 let memoryOne = [];
 let memoryTwo = [];
+let operator = [];
 
 function sum (a, b) {
     let sum = a + b;
@@ -36,6 +38,24 @@ function multiplication(a, b) {
 }
 
 function division(a, b) {
+    if (a == 0 || b == 0) {
+        displayText.textContent = 'ERROR!';
+        return ('ERROR!')}
     let division = a / b; 
     return division;
+}
+
+function operate (num1, operator, num2) {
+    if (operator == '+') {
+        return sum(num1, num2);
+    }
+    if (operator == '-') {
+        return subtration(num1, num2);
+    }
+    if (operator == '*'){
+        return multiplication(num1, num2);
+    }
+    if (operator == '/') {
+        return division(num1, num2);
+    }
 }
