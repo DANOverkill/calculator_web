ac91f7331f491454cd92ed10ba87ae4bc039f876
Daniel <danielz.nas@gmail.com>
2022-03-13 16:10:13 -0300

    added conditions for memoryOne number

--- script.js
+++ script.js
@@ -4,11 +4,73 @@ const clearButton = document.querySelector('#clear');
 
 //    -------            UI           ------
 window.addEventListener('keydown', (e) => {
-    if(!e.code.includes('Numpad')) {return}
-    // if(e.keyCode < 96 || e.keyCode > 105) {return}
-    memoryOne.push(e.key)
-    displayText.textContent = memoryOne.join('');
-    console.log(e.keyCode);
+    if(e.code.includes('Numpad')) {
+        if (memoryOne.length == 0 && operator.length == 0) {
+            if (e.key == '0' || e.key == '+' || e.key == '*' || e.key == '/' || e.key == 'Enter') {return}
+            else if (e.key == '.') {
+                memoryOne.push('0.')
+                displayText.textContent = memoryOne.join(''); 
+                console.log('condition (memoryOne.length == 0 - e.key == .) was triggered');
+            }
+            else if (e.key == '-') {
+                memoryOne.push(e.key)
+                displayText.textContent = memoryOne.join(''); 
+                console.log('condition (memoryOne.length == 0 - e.key == -) was triggered'); 
+            }
+            else {
+                memoryOne.push(e.key)
+                displayText.textContent = memoryOne.join('');
+                console.log('condition (memoryOne.length == 0 ELSE) was triggered');
+            }
+        }
+        else if (memoryOne.length > 0 && memoryOne[0] == '-' && memoryOne.length < 10 && operator.length == 0) {
+            if (!memoryOne.includes('.') && !memoryOne.includes('0.')) {
+                if (e.key == '0') {return}
+                else if (e.key == '.') {
+                    memoryOne.push('0.')
+                    displayText.textContent = memoryOne.join(''); 
+                    console.log('condition (memoryOne.length == 0 - e.key == .) was triggered');
+                }
+                else {
+                memoryOne.push(e.key)
+                displayText.textContent = memoryOne.join('');
+                console.log('condition (memoryOne.length > 0) was triggered');
+                }
+            }
+            else if (memoryOne.includes('0.') || memoryOne.includes('.')) {
+                if (e.key == '.') {return}
+                else {
+                    memoryOne.push(e.key)
+                    displayText.textContent = memoryOne.join('');
+                    console.log('condition (memoryOne.length > 0) was triggered');    
+                }
+                   
+            }
+        }
+        else if (memoryOne.length > 0 && memoryOne[0] != '-' && memoryOne.length < 10 && operator.length == 0) {
+            if (!memoryOne.includes('.') && !memoryOne.includes('0.')) {
+                memoryOne.push(e.key)
+                displayText.textContent = memoryOne.join('');
+                console.log('condition (memoryOne.length > 0) was triggered');
+            }
+            else if (memoryOne.includes('0.') || memoryOne.includes('.')) {
+                if (e.key == '.') {return}
+                else {
+                    memoryOne.push(e.key)
+                    displayText.textContent = memoryOne.join('');
+                    console.log('condition (memoryOne.length > 0) was triggered');    
+                }
+                   
+            }
+            // else if (e.key == '/' || e.key == '*' || e.key == '-' || e.key == '+') {
+            //     operator.push(e.key)
+            //     console.log(operator)
+            // }
+            
+        }
+        
+    }
+    
 })
 
 clearButton.addEventListener('click', () => {
